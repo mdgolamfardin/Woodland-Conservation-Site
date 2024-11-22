26952811e45b90dbc0298657ffbe282d38ae0a0e
mdgolamfardin <69181182+mdgolamfardin@users.noreply.github.com>
2024-11-22 16:12:00 -0400

    More styles to gallery, added funtionalities.
    - added bow shadow to image cards.
    - created a function on layers name creatEImageCards to create all the images for available data
    -create data files for the gallery and stubbed them.

---
+++ src/data/faunaData.js
@@ -0,0 +1,64 @@
+var loremIpsum =
+  "Lorem ipsum odor amet, consectetuer adipiscing elit. Dis interdum diamfinibus elit quam class per cras? Massa luctus ultrices quam portaturpis proin. Tristique euismod nec ornare blandit posuere non.";
+
+var picsum = "https://picsum.photos/400/300"
+
+const faunaData = [
+    {
+        id: 1,
+        url: picsum,
+        title: "title",
+        description: loremIpsum,        
+    },
+    {
+        id: 2,
+        url: picsum,
+        title: "title",
+        description: loremIpsum,        
+    },
+    {
+        id: 3,
+        url: picsum,
+        title: "title",
+        description: loremIpsum,        
+    },
+    {
+        id: 4,
+        url: picsum,
+        title: "title",
+        description: loremIpsum,        
+    },
+    {
+        id: 5,
+        url: picsum,
+        title: "title",
+        description: loremIpsum,        
+    },
+    {
+        id: 6,
+        url: picsum,
+        title: "title",
+        description: loremIpsum,        
+    },
+    {
+        id: 7,
+        url: picsum,
+        title: "title",
+        description: loremIpsum,        
+    },
+    {
+        id: 8,
+        url: picsum,
+        title: "title",
+        description: loremIpsum,        
+    },
+    {
+        id: 9,
+        url: picsum,
+        title: "title",
+        description: loremIpsum,        
+    },
+];
+
+export default faunaData;
+
